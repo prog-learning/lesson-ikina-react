f788ad71f2b08c8089d5274d4195c71b003eacd5
nbr41to <nbr.41to@gmail.com>
2021-05-20 00:50:04 +0900

    .

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 
 ## Lesson07
 
-バリデーションをしよう
+フォームにバリデーション機能をつけよう
 
 - [ ] create button の onClick の処理は,addTodo という名前の関数を作り,それを実行するようにする
 - [ ] input に何も入力されていないときは todo を追加できないようにし,「入力してください！」と alert を表示する
 - [ ] todo に追加したあとは input の中身を空にする
 
-※今回の機能は useState を学ぶためのもので,TODOLIST 作りには影響はないので,消しても OK
+## 学べる知識
+
+- if 文
+- 関数
+
 ※完成コードは[次の Lesson](https://github.com/prog-learning/lesson-ikina-react/tree/lesson08)
--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import List from './List';
 
 const App = () => {
   const [title, setTitle] = useState("JUST DO IT.");
-  const [inputText, setInputText] = useState("");
   const [todoList, setTodoList] = useState([
     'プログラミングを勉強する',
     '運動をして生産性を上げる',
     '宝くじを当てる',
   ]);
+  const [inputText, setInputText] = useState(""); // inputの中身を管理する変数を新たに追加
+
+  console.log(inputText); // 入力値が変更する度にsetInputTextでinputTextが変更されていることをConsoleで確認
 
   return (
     <div>
@@ -16,8 +18,8 @@ const App = () => {
       <button onClick={() => setTitle('TO DO LIST.')}>change title</button>
       <div>
         <h3>Create new todo</h3>
-        <input type="text" onChange={(e) => setInputText(e.target.value)} />
-        <button onClick={() => setTodoList([...todoList, inputText])}>create</button>
+        <input type="text" onChange={(e) => setInputText(e.target.value)} /> {/* onChangeイベントで入力に変更がある度にinputTextを変更 */}
+        <button onClick={() => setTodoList([...todoList, inputText])}>create</button> {/* スプレッド構文で,「今の配列の要素+新たに追加するtodo」に変更 */}
       </div>
       <List todoList={todoList} />
     </div>
