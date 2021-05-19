7eceffa340064e84f28a16d21de0ec139db17383
nbr41to <nbr.41to@gmail.com>
2021-05-19 20:44:50 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
 const App = () => {
   return (
-    <h1>Hello React!!</h1>
+    <div>
+      <h1>JUST DO IT.</h1>
+      <div>
+        <h3>Create nex todo</h3>
+        <input type="text" />
+        <button>create</button>
+      </div>
+      <ul>
+        <li>
+          <input type="checkbox" />
+          <span>皿洗い</span>
+        </li>
+        <li>
+          <input type="checkbox" />
+          <span>筋トレ</span>
+        </li>
+        <li>
+          <input type="checkbox" />
+          <span></span>
+        </li>
+      </ul>
+    </div>
   );
 };
 
