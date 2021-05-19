1f71ed08d6e5f0fb636354abe54c680a025a8019
nbr41to <nbr.41to@gmail.com>
2021-05-20 00:42:11 +0900

    .

--- src/List.jsx
+++ src/List.jsx
@@ -3,8 +3,8 @@ import Item from './Item';
 const List = ({ todoList }) => {
   return (
     <ul style={{ listStyle: 'none' }}>
-      {todoList.map((todo) => { // JSX内でJavaScriptを書く際は{ }を忘れずに
-        return <Item content={todo} />;
+      {todoList.map((todo, index) => { // JSX内でJavaScriptを書く際は{ }を忘れずに
+        return <Item key={index} content={todo} />; // keyはReactのエラーを解消するためのもの
       })}
       {/* このmapメソッドは最初は難しく感じるので,無理して理解しなくてもOK */}
     </ul>
