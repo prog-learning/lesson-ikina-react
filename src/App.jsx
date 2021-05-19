167d7f239da6e8e99af1815f31258718bf3e59fc
nbr41to <nbr.41to@gmail.com>
2021-05-20 00:12:39 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-
-
 const App = () => {
 
   return (
     <div>
-      <h1>JUST DO IT.</h1>
+      <h1>JUST DO IT.</h1> {/* タイトルは好きに変更 */}
       <div>
-        <h3>Create nex todo</h3>
+        <h3>Create new todo</h3>
         <input type="text" />
         <button>create</button>
       </div>
-      <ul style={{ listStyle: 'none' }}>
-        <List content='プログラミングを勉強する' />
-        <List content='運動をして生産性を上げる' />
-        <List content='宝くじを当てる' />
+      <ul>
+        <li>
+          <input type="checkbox" /> {/* input のtype属性には他にも項目があるので調べておくと良い */}
+          <span>プログラミングを勉強する</span>
+        </li>
+        <li>
+          <input type="checkbox" />
+          <span>運動をして生産性を上げる</span>
+        </li>
+        <li>
+          <input type="checkbox" />
+          <span>宝くじを当てる</span>
+        </li>
       </ul>
     </div>
   );
